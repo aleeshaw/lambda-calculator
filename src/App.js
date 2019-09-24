a3fc5ca650bf09a9f5917b2a6d720912fcae7aaf
aleesha-wood <spectrat@spectratart.com>
2019-09-24 01:09:14 -0400

    added display to app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import "./App.css";
 import Logo from "./components/DisplayComponents/Logo";
 import Numbers from "./components/ButtonComponents/NumberButtons/Numbers.js";
 import Specials from "./components/ButtonComponents/SpecialButtons/Specials.js";
-import Operators from "./components/ButtonComponents/OperatorButtons/Operators.js"
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators.js";
+import Display from "./components/DisplayComponents/Display.js";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -22,6 +23,7 @@ function App() {
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */
         <>
+        <Display />
         <Specials />
         <Numbers />
         <Operators />
